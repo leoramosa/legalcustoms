3990279ab5c6012fed65e6039dd1c75fb8ae0e32
Leonardo Ramos <leo.ramos.ari@gmail.com>
2021-02-08 15:16:15 -0500

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './styles/about.css'
+import imgInfo from '../images/about_only2.png'
+
+ const Home = () => {
+  return (
+    <>
+      <div className="content_about">
+        <div className="about-header">
+          <div className="about_overlay">
+          <div className="about_container">
+              <p className="about_title">Nuestra <strong>Historia</strong></p>
+              <p className="about_info">Nuestra experiencia en el campo de la Geología </p>
+              <br/>
+              <a className="" href="">
+                <button className="contact_link" type="button">
+                Contáctanos
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="about_information">
+          <div className="row_about">
+            sfsjl sdflsjdfl sdfjlsdfj sdlfjsld
+          </div>
+          <div className="row_about">
+            <div className="">
+              imagen
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home;
