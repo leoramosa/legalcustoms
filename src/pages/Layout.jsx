e642805c8c354a2f707672837d61952b539fb0a4
Leonardo Ramos <leo.ramos.ari@gmail.com>
2021-03-11 20:59:57 -0500

    chat messenger

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import NavBar from './../components/NavBar';
 import Footer from './../components/Footer';
 import './styles/layout.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 const Layout = ( {children} ) => {
   return (
     <div className="layout-global">
       <NavBar/>
+      <MessengerCustomerChat
+    pageId="103663745077318"
+    appId="775872919759099"
+    /* htmlRef="<REF_STRING>" */
+  />
       <div className="layout-content">
         {children}
       </div>
